304bf144c0937fd314edfe67a2bfcbee60153f5d
AndreOlivers <andre.ce.olive@gmail.com>
2022-11-26 17:13:20 -0300

    Configurações para implementar o token do JWT

---
+++ Aluno.Application/Aluno.Domain/Security/SgningConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Domain.Security
+{
+    public class SgningConfigurations
+    {
+        public SecurityKey Key { get; set; }
+        public SigningCredentials SigningCredentials { get; set; }
+
+        public SgningConfigurations()
+        {
+            using (var provider = new RSACryptoServiceProvider(2048))
+            {
+                Key = new RsaSecurityKey(provider.ExportParameters(true));
+            }
+
+            SigningCredentials = new SigningCredentials(Key , SecurityAlgorithms.RsaSha256Signature);
+        }
+    }
+}
